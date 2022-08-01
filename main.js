f7eaf18b112ff2e7b2e5f10e3f4a7b14bb60eb06
Lucas Mariano dos Santos <90803753+lmariano1308@users.noreply.github.com>
2022-08-01 15:51:31 -0300

    Final Project of MIDI - JavaScript
    Basic learning on how to use javascript in a simple way on the web, assigning sounds to buttons, making them dynamic and responsive, I created a MIDI using HTML5/CSS3

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomClap (){
-    document.querySelector('.tecla_clap');
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    document.querySelector('#som_tecla_clap').play();
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; 
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
